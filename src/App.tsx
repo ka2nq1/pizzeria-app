55321ba905121bc74abd499ac34e61c9f5f781eb
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-06-13 18:10:07 +0300

    project structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+let pizza: string = 'Sausage';
+
+let arr: string[] = ['asd']
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Title</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pizzasList.js
@@ -0,0 +1,10 @@
+const pizzasList = [
+    {title: 'Papperoni', price: 220, img: 'pizza-1.jpg', id: 1},
+    {title: 'Margarita', price: 200, img: 'pizza-2.jpg', id: 2},
+    {title: 'Saussage', price: 231, img: 'pizza-3.jpg', id: 3},
+    {title: '3 Cheese', price: 230, img: 'pizza-4.jpg', id: 4},
+    {title: 'Veggie', price: 260, img: 'pizza-5.jpg', id: 5},
+    {title: 'Mixed', price: 280, img: 'pizza-6.jpg', id: 6},
+];
+
+export default pizzasList;
